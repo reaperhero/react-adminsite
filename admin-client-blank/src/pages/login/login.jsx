3bea07baa03c001da525b3901ba4ea52fd22fc1f
chenqiangjun <chenqiangjun@mudu.tv>
2020-08-05 00:02:03 +0800

    静态组件

--- admin-client-blank/src/pages/login/login.jsx
+++ admin-client-blank/src/pages/login/login.jsx
@@ -1,12 +1,54 @@
 import React, {Component} from 'react'
+import {
+    Form,
+    Icon,
+    Input,
+    Button,
+    message
+} from 'antd'
+
+import './login.less'
+import logo from './images/logo.png'
+
+const Item = Form.Item // 不能写在import之前
 
 export default class Login extends Component {
 
-  render () {
-    return (
-      <div>
-        <p className='title'>Login</p>
-      </div>
-    )
-  }
+    handleSubmit = () => {
+        message.success("sues")
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>React后台管理</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登陆</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                            <Input
+                                prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                placeholder="用户名"
+                            />
+                        </Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登陆
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
+        )
+    }
 }
